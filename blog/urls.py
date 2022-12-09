c7b2670a6ef7b34d47c306925a938530cc3b6353
Navid Kalashi <kalashinavid@gmail.com>
2022-12-09 17:14:00 +0330

    like added

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 # from . import views
-from .views import HomeView, ArticleDetailView, AddPostView, UpdatePostView, DeletePostView, AddCategorytView, categoryView, categoryListView
+from .views import HomeView, ArticleDetailView, AddPostView, UpdatePostView, DeletePostView, AddCategorytView, categoryView, categoryListView, LikeView
 
 urlpatterns = [
     # path('', views.home, name='home')
@@ -12,4 +12,5 @@ urlpatterns = [
     path('article/delete/<int:pk>/', DeletePostView.as_view(), name='delete_post'),
     path('category/<str:cats>/', categoryView, name='category'),
     path('category-list/', categoryListView, name='category_list'),
+    path('like/<int:pk>/', LikeView, name='like_post'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
-from django.urls import reverse_lazy
+from django.urls import reverse_lazy, reverse
+from django.http import HttpResponseRedirect
 
 from .models import Post, Category
 from .forms import PostForm, EditPost
@@ -43,6 +44,10 @@ class ArticleDetailView(DetailView):
     def get_context_data(self, *args, **kwargs):
         cat_menu = Category.objects.all()
         context = super(ArticleDetailView, self).get_context_data(*args, **kwargs)
+
+        stuff = get_object_or_404(Post, id=self.kwargs['pk'])
+        total_likes = stuff.total_likes()
+        context['total_likes'] = total_likes
         context['cat_menu'] = cat_menu
         return context
     
@@ -73,3 +78,9 @@ class DeletePostView(DeleteView):
     model = Post
     template_name = 'delete_post.html'
     success_url = reverse_lazy('home')
+
+# Like
+def LikeView(request, pk):
+    post = get_object_or_404(Post, id=request.POST.get('post_id'))
+    post.likes.add(request.user)
+    return HttpResponseRedirect(reverse('article_detail', args=[str(pk)]))
