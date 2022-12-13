1e6f082af799eff8b78e5f786328d851bccd4778
Navid Kalashi <kalashinavid@gmail.com>
2022-12-13 17:01:03 +0330

    ckeditor added

--- blog/models.py
+++ blog/models.py
@@ -4,6 +4,8 @@ from django.urls import reverse
 
 from datetime import datetime, date
 
+from ckeditor.fields import RichTextField
+
 # Create your models here.
 
 class Category(models.Model):
@@ -20,7 +22,8 @@ class Post(models.Model):
     title = models.CharField(max_length=255)
     title_tag = models.CharField(max_length=255)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
-    body = models.TextField()
+    body = RichTextField(blank=True, null=True)
+    # body = models.TextField()
     date = models.DateField(auto_now_add=True)
     category = models.CharField(max_length=255, default='coding')
     likes = models.ManyToManyField(User, related_name='blog_posts')
